cd27687e28089de229b755c74fb8b72722e1483f
YJ Soon <yjsoon@tinkertanker.com>
2024-07-06 08:02:58 +0800

    Initial Commit

---
+++ testongging/testonggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testonggingApp.swift
+//  testongging
+//
+//  Created by YJ Soon on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testonggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
